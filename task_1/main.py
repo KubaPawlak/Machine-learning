a829880f6147aff24c2bec162b3a627a72caface
Jakub Pawlak <kuba_pawlak@icloud.com>
2024-11-11 18:59:18 +0100

    Implement score prediction in main file

--- task_1/main.py
+++ task_1/main.py
@@ -1,14 +1,36 @@
-from pathlib import Path
 import logging
+from pathlib import Path
 
-from data.movie import task
+from data.movie import task, train
+from task_1.classifier import KNeighborsClassifier
+from task_1.movie import Movie
+from task_1.movie.tmdb.client import Client
 
 _RESULT_FILE = Path('task_1_result.csv').absolute()
+import pandas as pd
+
+tmdb_client = Client()
 
 
 def predict_score(user_id: int, movie_id: int) -> int:
-    # todo: provide actual implementation
-    return user_id - movie_id
+    def distance_function(movie_1: Movie, movie_2: Movie) -> float:
+        # todo: specify actual distance function
+        if movie_1.title == movie_2.title:
+            return 0.0
+        else:
+            return 1.0
+
+    classifier = KNeighborsClassifier(n_neighbors=5, distance_function=distance_function)
+    movie_to_predict = tmdb_client.get_movie(movie_id)
+
+    watched_movies_ratings = train[train['UserID'] == user_id]
+
+    # get list of features for the movies watched by the user
+    watched_movies: pd.Series[int] = watched_movies_ratings['MovieID']
+    assert movie_id not in watched_movies
+    watched_movies: list[Movie] = [tmdb_client.get_movie(movie_id) for movie_id in watched_movies]
+
+    return classifier.fit_predict(watched_movies, watched_movies_ratings['Rating'], movie_to_predict)
 
 
 def main() -> None:
