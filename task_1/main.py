621f2b4241e4854019ba2148e7c1b35a9241d035
Jakub Pawlak <kuba_pawlak@icloud.com>
2024-11-11 19:56:11 +0100

    Implement main

--- task_1/main.py
+++ task_1/main.py
@@ -5,6 +5,7 @@ from data.movie import task, train
 from task_1.classifier import KNeighborsClassifier
 from task_1.movie import Movie
 from task_1.movie.tmdb.client import Client
+from features.movie_similarity import calculate_movie_similarity
 
 _RESULT_FILE = Path('task_1_result.csv').absolute()
 import pandas as pd
@@ -13,12 +14,11 @@ tmdb_client = Client()
 
 
 def predict_score(user_id: int, movie_id: int) -> int:
+
+    logging.debug("Predicting rating for movie %i by user %i", movie_id, user_id)
+
     def distance_function(movie_1: Movie, movie_2: Movie) -> float:
-        # todo: specify actual distance function
-        if movie_1.title == movie_2.title:
-            return 0.0
-        else:
-            return 1.0
+        return 1 / calculate_movie_similarity(movie_1, movie_2)
 
     classifier = KNeighborsClassifier(n_neighbors=5, distance_function=distance_function)
     movie_to_predict = tmdb_client.get_movie(movie_id)
@@ -27,14 +27,18 @@ def predict_score(user_id: int, movie_id: int) -> int:
 
     # get list of features for the movies watched by the user
     watched_movies: pd.Series[int] = watched_movies_ratings['MovieID']
-    assert movie_id not in watched_movies
+    assert movie_id not in watched_movies.values
+    logging.debug("Gathering movies watched by user %i...", user_id)
     watched_movies: list[Movie] = [tmdb_client.get_movie(movie_id) for movie_id in watched_movies]
 
-    return classifier.fit_predict(watched_movies, watched_movies_ratings['Rating'], movie_to_predict)
+    logging.debug("Generating prediction")
+    predicted_rating =  classifier.fit_predict(watched_movies, watched_movies_ratings['Rating'].to_numpy(), movie_to_predict)
+    logging.info("Predicted rating of movie %i by user %i: %i", movie_id, user_id, predicted_rating)
+    return predicted_rating
 
 
 def main() -> None:
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
 
     logging.info("Calculating predictions...")
     # apply the prediction function to each row in the task dataframe
