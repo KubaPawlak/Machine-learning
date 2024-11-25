7aa1a3148e7aea0572e7ec9029197a269f1fb8c9
Jakub Pawlak <kuba_pawlak@icloud.com>
2024-11-25 12:09:21 +0100

    Create different cases for decision tree choice

--- task_2/classification/decision_tree/choice.py
+++ task_2/classification/decision_tree/choice.py
@@ -1,14 +1,71 @@
+from abc import ABC, abstractmethod
+
 from . import Movie
 
 
-class Choice:
+class Choice(ABC):
     """
     Class representing a single choice performed by the decision tree
     """
 
-    def __init__(self, feature_name: str):
+    @abstractmethod
+    def test(self, movie: Movie) -> bool:
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+
+class ScalarChoice(Choice):
+    """Represents choice made on scalar features (e.g. movie duration).
+    Returns true if feature is greater or equal to the given value."""
+
+    def __init__(self, feature_name: str, value: int | float):
         self.feature_name = feature_name
+        self.value = value
 
     def test(self, movie: Movie) -> bool:
-        feature = movie[self.feature_name]
-        # todo: Implement testing
+        return movie[self.feature_name] >= self.value
+
+    def __str__(self):
+        return f"{self.feature_name} >= {self.value}"
+
+    def __repr__(self):
+        return f"ScalarChoice({self.feature_name}, {self.value})"
+
+
+class CaregoricalChoice(Choice):
+    """Choice made on categorical features (e.g. language).
+    Returns true if the feature is the same as tested value."""
+
+    def __init__(self, feature_name: str, value):
+        self.feature_name = feature_name
+        self.value = value
+
+    def test(self, movie: Movie) -> bool:
+        return movie[self.feature_name] == self.value
+
+    def __str__(self):
+        return f"{self.feature_name} = {self.value}"
+
+    def __repr__(self):
+        return f"CaregoricalChoice({self.feature_name}, {self.value})"
+
+
+class SetContainsChoice(Choice):
+    """Choice made on multivalued features (e.g. cast).
+    Checks if the tested value is contained in the feature."""
+
+    def __init__(self, feature_name: str, value):
+        self.feature_name = feature_name
+        self.value = value
+
+    def test(self, movie: Movie) -> bool:
+        return self.value in set(movie[self.feature_name])
+
+    def __str__(self):
+        return f"{self.value} in {self.feature_name}"
+
+    def __repr__(self):
+        return f"SetContainsChoice({self.feature_name}, {self.value})"
