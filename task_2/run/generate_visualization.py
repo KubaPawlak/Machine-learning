d314b3f3431093dcfde23306619164a1247bf4cd
Jakub Pawlak <kuba_pawlak@icloud.com>
2024-11-27 20:07:11 +0100

    Fix dot generation

--- task_2/run/generate_visualization.py
+++ task_2/run/generate_visualization.py
@@ -1,22 +1,28 @@
 import logging
 from pathlib import Path
 
+import numpy as np
+
 from task_2.classification.decision_tree import DecisionTree
 from task_2.visualisation import generate_graphviz_file
+
+from task_2.decision_tree_main import get_movie_features, train_decision_tree
+
+from data.movie import train
 
 DOT_FILE_PATH = Path(__file__).parent.absolute() / 'tree.dot'
 DEFAULT_USER_ID: int = 1642
 
 
-def generate_tree_for_user(user_id: int = DEFAULT_USER_ID) -> DecisionTree:
-    # todo: tree training (imported from another file once it is done)
-    logging.info("Generating tree for user %s", user_id)
-    pass
+def _generate_tree_for_user(user_id: int = DEFAULT_USER_ID) -> DecisionTree:
+    user_watched_movies = train[train['UserID'] == user_id]
+    movie_features = get_movie_features(np.unique(user_watched_movies['MovieID']))
+    tree = train_decision_tree(user_watched_movies, movie_features)
+    return tree
 
-
-if __name__ == '__main__':
+def _main():
     logging.basicConfig(level=logging.INFO)
-    tree = generate_tree_for_user()
+    tree = _generate_tree_for_user()
     logging.info("Generating dot file")
     dot = generate_graphviz_file(tree)
 
@@ -26,4 +32,7 @@ if __name__ == '__main__':
     DOT_FILE_PATH.write_text(dot)
     logging.info("Dot file successfully written to %s. Run 'dot -Tpng %s' to generate the image file.",
                  str(DOT_FILE_PATH), str(DOT_FILE_PATH))
+
+if __name__ == '__main__':
+    _main()
 
--- task_2/visualisation.py
+++ task_2/visualisation.py
@@ -4,7 +4,7 @@ def _visit_node(tree: DecisionTree) -> str:
     node_id = id(tree)
     if tree.leaf_value is not None:
         # is a leaf node
-        return f'{node_id} [label="{str(tree.leaf_value)}\n"'
+        return f'{node_id} [label="{str(tree.leaf_value)}"]\n'
 
     # is a choice node
     true_node_id = id(tree.child_success)
@@ -12,7 +12,7 @@ def _visit_node(tree: DecisionTree) -> str:
     output = ""
     output += f'{node_id} [label="{str(tree.choice)}"]\n'
     output += f'{node_id} -> {true_node_id} [label="true"]\n'
-    output += f'{node_id} -> {false_node_id} [label="true"]\n'
+    output += f'{node_id} -> {false_node_id} [label="false"]\n'
     output += _visit_node(tree.child_success)
     output += _visit_node(tree.child_fail)
     return output
