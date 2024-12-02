8bae4873c8e0712d66e756eec227ce4b175cabf1
Jakub Pawlak <kuba_pawlak@icloud.com>
2024-12-02 18:04:24 +0100

    Clean up testing code

--- task_3/main.py
+++ task_3/main.py
@@ -1,8 +1,9 @@
 import logging
 from pathlib import Path
 
+import pandas as pd
+
 from data.movie import train, task
-from task_3.cross_validation import kfold_cross_validation
 from task_3.prediction import predict_rating
 from task_3.similarity import similarity_function
 
@@ -10,19 +11,26 @@ logger = logging.getLogger(__name__)
 
 SUBMISSION_PATH = Path(__file__).parent / 'submission/submission.csv'
 
+def predict_ratings(users_and_movies: pd.DataFrame, similarity_func, rating_matrix: pd.DataFrame) -> list[int]:
+    predicted_ratings = []
+
+    for _, row in users_and_movies.iterrows():
+        target_user = row['UserID']
+        target_movie = row['MovieID']
+        predicted_rating = predict_rating(target_user, target_movie, similarity_func,
+                                          rating_matrix)
+        predicted_rating = int(round(predicted_rating))
+        assert 0 <= predicted_rating <= 5
+        predicted_ratings.append(predicted_rating)
+
+    return predicted_ratings
+
 def main():
     logging.basicConfig(level=logging.INFO)
 
     rating_matrix = train.pivot(index='UserID', columns='MovieID', values='Rating')
 
-    predicted_ratings = []
-    for _, row in task.iterrows():
-        target_user = row['UserID']
-        target_movie = row['MovieID']
-        predicted_rating = predict_rating(target_user, target_movie, similarity_function, rating_matrix)
-        predicted_rating = int(round(predicted_rating))
-        assert 0 <= predicted_rating <= 5
-        predicted_ratings.append(predicted_rating)
+    predicted_ratings = predict_ratings(task, similarity_function, rating_matrix)
 
     # Replace null values in task.csv with predicted ratings
     task['Rating'] = predicted_ratings
@@ -32,6 +40,7 @@ def main():
     task.to_csv(SUBMISSION_PATH, index=False, sep=';')
 
     logger.info(f"Predictions saved to {SUBMISSION_PATH}")
+
 
 if __name__ == "__main__":
     main()
--- task_3/test/test_solution.py
+++ task_3/test/test_solution.py
@@ -1,76 +1,58 @@
 import logging
+from typing import Tuple, Any
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, accuracy_score
 from sklearn.model_selection import train_test_split
 
-from data.movie import train
-from task_3.prediction import predict_rating
+from data.movie import train as whole_train
+from task_3.main import predict_ratings
 from task_3.similarity import similarity_function
 
 logger = logging.getLogger(__name__)
 
-def split_data(train_data):
+
+def split_data(train_data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
     train_test, test_test = train_test_split(train_data, test_size=0.2, random_state=42)
     return train_test, test_test
 
 
-def replace_ratings_with_nan(test_test):
-    test_test_with_nulls = test_test.copy()
-    test_test_with_nulls['Rating'] = np.nan
-    return test_test_with_nulls
+def clear_ratings(data: pd.DataFrame, in_place: bool = False) -> pd.DataFrame:
+    if not in_place:
+        data = data.copy()
+    data['Rating'] = np.nan
+    return data
 
 
-def predict_for_test_set(test_set, similarity_func, rating_matrix_train):
-    predicted_ratings = []
+def calculate_metrics(y_true: list[float], y_pred: list[float]) -> float:
+    assert len(y_true) == len(y_pred)
 
-    for _, row in test_set.iterrows():
-        target_user = row['UserID']
-        target_movie = row['MovieID']
-        predicted_rating = predict_rating(target_user, target_movie, similarity_func,
-                                          rating_matrix_train)
-        predicted_ratings.append(predicted_rating)
-
-    return predicted_ratings
-
-def calculate_accuracy(real_ratings, test_test_with_nulls, predicted_ratings):
-    test_test_with_nulls['Rating'] = predicted_ratings
-
-    comparison_df = pd.merge(real_ratings, test_test_with_nulls[['UserID', 'MovieID', 'Rating']], on=['UserID', 'MovieID'], how='left')
-
-    # Calculate MAE and RMSE
-    mae = mean_absolute_error(comparison_df['Rating_x'], comparison_df['Rating_y'])
-    rmse = np.sqrt(mean_squared_error(comparison_df['Rating_x'], comparison_df['Rating_y']))
+    mae = mean_absolute_error(y_true, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+    accuracy = accuracy_score(y_true, y_pred)
 
     logger.info(f"Mean Absolute Error (MAE): {mae:.4f}")
     logger.info(f"Root Mean Squared Error (RMSE): {rmse:.4f}")
-
-    # Calculate accuracy as percentage of correct predictions
-    correct_predictions = comparison_df[comparison_df['Rating_x'] == comparison_df['Rating_y']].shape[0]
-    total_predictions = comparison_df.shape[0]
-    accuracy_percentage = (correct_predictions / total_predictions) * 100
-
-    logger.info(f"Accuracy: {accuracy_percentage:.2f}%")
+    logger.info(f"Accuracy: {round(accuracy * 100)}%")
+    return accuracy
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
     logger.info("Starting the process...")
 
-    train_test, test_test = split_data(train)
+    train, test = split_data(whole_train)
 
-    real_ratings = test_test[['UserID', 'MovieID', 'Rating']].copy()
+    task = clear_ratings(test, in_place=False)
 
-    test_test_with_nulls = replace_ratings_with_nan(test_test)
+    rating_matrix = train.pivot(index='UserID', columns='MovieID', values='Rating')
 
-    rating_matrix_train = train_test.pivot(index='UserID', columns='MovieID', values='Rating')
-
-    logger.info("Predicting ratings for test_test...")
-    predicted_ratings = predict_for_test_set(test_test_with_nulls, similarity_function, rating_matrix_train)
+    logger.info("Predicting ratings...")
+    predicted_ratings = predict_ratings(task, similarity_function, rating_matrix)
 
     logger.info("Calculating accuracy...")
-    calculate_accuracy(real_ratings, test_test_with_nulls, predicted_ratings)
+    calculate_metrics(test['Rating'], predicted_ratings)
 
 
 if __name__ == "__main__":
